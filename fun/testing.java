18931547c6043628c355eb46122348b34c007601
pranesh007A <ppranesh663@gmail.com>
2025-02-25 20:17:13 +0530

    commit

--- fun/testing.java
+++ fun/testing.java
@@ -1,20 +1,11 @@
 package fun;
+
+import java.util.Arrays;
+
 public class testing {
     public static void main(String[] args) {
-        int[]numbers={1,2,3,4,5};
-        int start=0;
-        int end = numbers.length-1;
-        while(start<end){
-            int temp = numbers[end];
-            numbers[start] = numbers[end];
-            numbers[end] = temp;
-        }
-        start ++;
-        end--;
-     
-     for(int num:numbers){
-        System.out.println(num+" ");
-     }
+        String[]clothing={"shirt","pant","tshirt"};
+         System.out.println("clothing section :"+(Arrays.toString(clothing)));
 
 }
 }
