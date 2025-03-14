256b7eeb66521bd0683fb93e5433ff825ad338c6
pranesh007A <ppranesh663@gmail.com>
2025-03-14 11:53:17 +0530

    yes

---
+++ String/substring.java
@@ -0,0 +1,11 @@
+
+package String;
+
+public class substring
+{
+	public static void main(String[] args) {
+	    String str1="hello friends";
+	 System.out.println(str1.substring(0));//here the number goes like(int start place value,int end place value).
+	                                                  //or can be like only the start value so that it prints till the last.    
+	}
+}
